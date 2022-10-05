401c80742e16825c40a250b038e6a4d1d4cba8ef
alvarohernandez-travelperk <alvaro.hernandez@travelperk.com>
2022-10-05 14:36:47 +0200

    Add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import RecipeItem from './Components/Recipe';
 import { useEffect, useState } from 'react';
+import { Route, Routes, Link } from 'react-router-dom';
+import AddRecipes from './Views/AddRecipes';
 import ListRecipes from './Views/ListRecipes';
 
 function App() {
-const [recipes, setRecipes] = useState<[]>([])
+  const [recipes, setRecipes] = useState<[]>([])
 
   useEffect(() => {
     console.log("Fetch data from backend")
@@ -14,9 +15,14 @@ const [recipes, setRecipes] = useState<[]>([])
     <div>
       <h1>Recipe APP</h1>
       <li>
+      <Link to="/add">
         <button>Add new recipe</button>
+        </Link>
       </li>
-      <ListRecipes />
+      <Routes>
+        <Route path="/" element={<ListRecipes />} />
+        <Route path="add" element={< AddRecipes/>} />
+      </Routes>
     </div>
   );
 }
--- src/Views/AddRecipes.tsx
+++ src/Views/AddRecipes.tsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function AddRecipes() {
   return (
     <div>
       <h2>Add new Recipes</h2>
+      <Link to="/">
+        <button>See all recipes</button>
+        </Link>
     </div>
   )
 }
